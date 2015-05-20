c58e6a0e6f1dab3e0ab8b65ad8e8c32362801a48
Jairo <jago86@gmail.com>
2015-05-20 16:38:03 -0500

    Añado soporte para manejar varios formularios en el mismo html. Añado metodos para el usuario. Encapsulo funciones privadas

--- src/jquery.stepyform.js
+++ src/jquery.stepyform.js
@@ -10,13 +10,12 @@
 (function( $ ) {
 	var methods = {
 		init : function() { 
+			var form=this
 			var counter=1, nextStep, prevStep
-			var numberSteps=this.children(".stepy-step").length
+			var numberSteps=form.children(".stepy-step").length
 			var options=arguments[0]
-			// console.log(opts)
-			// arguments[0].fun()
 
-			this.children(".stepy-step").each(function() {
+			form.children(".stepy-step").each(function() {
 				// add step number in div
 				var divStep = $( this );
 				divStep.addClass("stepy-step-" + counter)
@@ -60,16 +59,18 @@
 
 			});
 			// hide divs
-			this.children(".stepy-step").hide().addClass("off").filter(".stepy-step-1").show().removeClass("off").addClass("on")
+			form.children(".stepy-step").hide().addClass("stepy-step-off").filter(".stepy-step-1").show().removeClass("stepy-step-off").addClass("stepy-step-on")
 			//add event for call the steps in buttons
-			this.find(".stepy-next, .stepy-prev").on('click', function(event){
-				methods.goStep.apply(this)
+			form.find(".stepy-next, .stepy-prev").on('click', function(event){
 				event.preventDefault()
+				privateMethods._callStep.apply(this, form)
 			})
 		},
-		goStep : function() {
-			var stepGo=$(this).attr("data-stepy-call")
-			var stepFrom=methods._getStepFather.apply(this)
+		
+		goStep : function (step) {
+			var contextForm=this //formulario al que se hace referencia
+			var stepGo=step
+			var stepFrom=contextForm.find(".stepy-step-on").attr("data-stepy-step")
 			var classStepGo="stepy-step-" + stepGo
 			var classStepFrom= "stepy-step-" + stepFrom
 			var direction=["left", "right"]
@@ -79,50 +80,74 @@
 				{
 					direction=["right", "left"]
 				}
-				$("."+classStepFrom).toggle( "slide", 
+				contextForm.find("."+classStepFrom).toggle( "slide", 
 					{
 						direction: direction[0]
 					}, 
 					function(){ 
-						$( "."+classStepGo ).toggle( "slide", 
+						contextForm.find( "."+classStepGo ).toggle( "slide", 
 							{
 								direction: direction[1]
 							})
-						.addClass('on')
-						.removeClass('off')
-						opts.onChangeStep.call($( "."+classStepGo ).get(0))
+						.addClass('stepy-step-on')
+						.removeClass('stepy-step-off')
+						opts.onChangeStep.call(contextForm.find( "."+classStepGo ).get(0))
 				 	})
-				.removeClass('on')
-				.addClass('off')
+				.removeClass('stepy-step-on')
+				.addClass('stepy-step-off')
 			}
 		},
-		_getStepFather : function( element ) { 
-		  return $( this ).parents()
-			.map(function() {
-				if($(this).hasClass("stepy-step"))
-				{
-					return $(this).attr('data-stepy-step')
-				}
-			}).get().join()
+		goStepOfElement : function(selector) {
+			if($(selector).length)
+			{
+				$(selector).each(function(){
+					var stepFather=privateMethods._getStepFather.apply($(this))
+					var formFather=privateMethods._getFormFather.apply($(this))
+					methods.goStep.apply(formFather, [stepFather.attr("data-stepy-step")])
+				})
+			}
+			else
+			{
+				$.error(selector + " doesn't exist")
+			}
+		}
+	};
+
+	var privateMethods = {
+		_getStepFather : function() { 
+		  return $( this ).closest(".stepy-step")
+		},
+		_getFormFather : function() { 
+		  return $( this ).closest("form")
+		},
+		_callStep : function() {
+			var context=arguments[0]
+			var stepGo=$(this).attr("data-stepy-call") //this contiene el elemento jquery con el boton presionado
+			methods.goStep.apply($(context), [stepGo])
 		},
 	};
  
     $.fn.stepyform = function(method) {
-		// If function exist, call it
-		if ( methods[method] ) {
-			opts = $.extend( {}, $.fn.stepyform.defaults, Array.prototype.slice.call( arguments, 1 ))
-			methods[ method ].apply( this, opts)
-		} else if ( typeof method === 'object' || ! method ) {
-			//If there's no parameters
-			//or the parameter is a configuration object
-			//call init
-			opts = $.extend( {}, $.fn.stepyform.defaults, arguments[0])
-			methods.init.apply( this )
-		} else {
-			// Else, show error
-			$.error( method 
-			+ ' is not a jQuery.stepystep function' );
-		}  
+    		// If function exist, call it
+			if ( methods[method] ) {
+				var args = Array.prototype.slice.call( arguments, 1 )
+				this.each(function(){
+					methods[ method ].apply( $(this), args)
+				})
+			} else if ( typeof method === 'object' || ! method ) {
+				//If there's no parameters
+				//or the parameter is a configuration object
+				//call init
+				opts = $.extend( {}, $.fn.stepyform.defaults, arguments[0])
+				this.each(function(){
+					methods.init.apply( $(this) )
+				})
+				
+			} else {
+				// Else, show error
+				$.error( method 
+				+ ' is not a jQuery.stepystep function' );
+			} 
 		return this;
     };
 
